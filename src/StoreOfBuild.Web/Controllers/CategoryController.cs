e2944d1383206a0f6c4faf4df25e36a64b0e0a91
Luis Alfredo Stockler Filho <lastock@outlook.com.br>
2018-11-19 14:00:36 -0200

    Initial commit

---
+++ src/StoreOfBuild.Web/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreOfBuild.Web.Models;
+using StoreOfBuild.Domain.Dtos;
+using StoreOfBuild.Domain.Products;
+
+namespace StoreOfBuild.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CategoryStorer _categoryStorer;
+
+        public CategoryController(CategoryStorer categoryStorer)
+        {
+            _categoryStorer = categoryStorer;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateOrEdit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrEdit(CategoryDto dto)
+        {
+            _categoryStorer.Store(dto);
+            return View();
+        }
+
+    }
+}
